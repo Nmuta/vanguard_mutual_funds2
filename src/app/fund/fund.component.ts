fde8a1f75743395fe4efa004df43a8cc0067d0c6
Nmuta Jones <nmuta1@gmail.com>
2021-12-22 12:56:05 -0700

    Final edit in place code with TextArea

--- src/app/fund/fund.component.ts
+++ src/app/fund/fund.component.ts
@@ -29,7 +29,6 @@ export class FundComponent implements OnInit {
       console.log("this is my wildcard 'id' ", myid);
       this.fundService.getFund(myid).subscribe(payload=>{
         this.fund = payload;
-        const obj = {...this.fund};
         for(const [key, value] of Object.entries(this.fund)){
            const localarray = [key, value, false];
            this.tempFundList.push(localarray);
@@ -40,11 +39,31 @@ export class FundComponent implements OnInit {
 
 
   update(): void {
+
+    this.reconstituteFund();
+
     this.fundService.updateFund(this.fund).subscribe(data =>{
       if(data){
         this.router.navigateByUrl("/funds");
       }
     })
+  }
+
+  reconstituteFund(): void {
+    const reconstitutedFund: any = {};
+    this.tempFundList.forEach(item =>{
+      reconstitutedFund[item[0]] = item[1];
+    })
+    this.fund = reconstitutedFund;
+  }
+
+  makeEditable(index: number): void{
+    this.tempFundList[index][2] = true;
+  }
+
+  makeStatic(index: number): void{
+    console.log("blur has been fired")
+    this.tempFundList[index][2] = false;
   }
 
   
