5299ea2348dfaabd28dae46d7e1da4210d0e5ea5
Nmuta Jones <nmuta1@gmail.com>
2021-12-22 11:11:46 -0700

    Up to creating array of arrays

--- src/app/fund/fund.component.ts
+++ src/app/fund/fund.component.ts
@@ -12,6 +12,11 @@ export class FundComponent implements OnInit {
 
   fund:Fund = {};
    
+  // workingFund = {title: ["asdf", {editing: false}], 
+  //                subtitle: ["adsf", {editing: true}]
+  //               }
+
+  tempFundList:any[][] = [];
 
   constructor(private route:ActivatedRoute, 
     private fundService: FundService,
@@ -24,12 +29,22 @@ export class FundComponent implements OnInit {
       console.log("this is my wildcard 'id' ", myid);
       this.fundService.getFund(myid).subscribe(payload=>{
         this.fund = payload;
+        const obj = {...this.fund};
+        for(const [key, value] of Object.entries(this.fund)){
+           const localarray = [key, value, false];
+           this.tempFundList.push(localarray);
+        }
       })
     })
   }
 
-  edit() :void {
-     this.router.navigateByUrl(`/funds/${this.fund.id}/edit`);
+
+  update(): void {
+    this.fundService.updateFund(this.fund).subscribe(data =>{
+      if(data){
+        this.router.navigateByUrl("/funds");
+      }
+    })
   }
 
   
